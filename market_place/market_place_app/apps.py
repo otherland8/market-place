7d4d0bdc018b5c281789f5fff089020672afaf06
Alexander Cheshmedzhiev <otherland@abv.bg>
2016-05-24 21:46:02 +0300

    Setup Django project and app

---
+++ market_place/market_place_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MarketPlaceAppConfig(AppConfig):
+    name = 'market_place_app'
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+	name="market_place",
+	version="0.01",
+	packages="find_packages()",
+	author="Alexander Cheshmedzhiev",
+	description="A package for the marker_place project",
+	license="MIT",
+	keywords="market place market_place"
+)
